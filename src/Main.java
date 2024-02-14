ea3e970112b98a0e7dcdc59a8f6e9552baf952da
KevinWlk <kevin.walencik@gmail.com>
2024-02-14 17:01:14 +0100

    Swing init

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,44 @@
 import com.addresse.model.*;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        DbConnexion.getConnexion();
-
-        User userToDelete = new User();
-        userToDelete.setId(1);
-        boolean deleted = UserManager.deleteUserById(userToDelete);
-        if (deleted) {
-            System.out.println("Utilisateur supprimé avec succès !");
-        } else {
-            System.out.println("Échec de la suppression de l'utilisateur.");
-        }
-
-        ArrayList<User> allUsers = UserManager.getAllUser();
-        System.out.println("-----------------------------------------------------------------");
-        System.out.printf("| %-2s | %-10s | %-10s | %-30s | \n", "ID", "Nom", "Prénom", "Email");
-        System.out.println("-----------------------------------------------------------------");
-        for (User user : allUsers) {
-            System.out.printf("| %-2s | %-10s | %-10s | %-30s | \n",
-                    user.getId(), user.getNom(), user.getPrenom(), user.getEmail());
-        }
-        System.out.println("-----------------------------------------------------------------");
-
-
+        Exemple exemple = new Exemple(null);
+//        DbConnexion.getConnexion();
+//
+//        //Update un utilisateur
+//        User update = new User("Update2","update2","test@test.com","123456");
+//        //version avec boolean en sortie
+//        if(UserManager.updateUser(update).getEmail() != null){
+//            System.out.println("le compte "+ UserManager.updateUser(update).getEmail() +" a été mis à jour en BDD");
+//        }
+//        else{
+//            System.out.println("Le compte n'existe pas");
+//        }
+//
+//        //Supprimer un utilisateur par son ID
+//        User userToDelete = new User();
+//        userToDelete.setId(1);
+//        boolean deleted = UserManager.deleteUserById(userToDelete);
+//        if (deleted) {
+//            System.out.println("Utilisateur supprimé avec succès !");
+//        } else {
+//            System.out.println("Échec de la suppression de l'utilisateur.");
+//        }
+//
+//        //Afficher la liste des utilisateurs
+//        ArrayList<User> allUsers = UserManager.getAllUser();
+//        System.out.println("-----------------------------------------------------------------");
+//        System.out.printf("| %-2s | %-10s | %-10s | %-30s | \n", "ID", "Nom", "Prénom", "Email");
+//        System.out.println("-----------------------------------------------------------------");
+//        for (User user : allUsers) {
+//            System.out.printf("| %-2s | %-10s | %-10s | %-30s | \n",
+//                    user.getId(), user.getNom(), user.getPrenom(), user.getEmail());
+//        }
+//        System.out.println("-----------------------------------------------------------------");
+//
+//
     }
 }
